3202ed799ee5e033059be27810dbe48b9a23aa7f
Daniel Moura <daniel.moura@driven.cx>
2020-04-25 13:07:37 -0300

    Configuring application to deploy on Heroku

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send({ status: 'ok' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('Application is running on port ', PORT);
+});
